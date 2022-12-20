6d1f3c8ae2fca416a84a44b6b882e2f8231a950d
miftajannat <miftajannat.dev@gmail.com>
2022-12-20 10:08:57 +0600

    react app deploying to github pages

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'badhruddouza@gmail.com',
+    // phone: '+915588776600',
+    // address: 'Menlo Park, California, United States - 673822 ',
+
+    sheetAPI: ''
+}
